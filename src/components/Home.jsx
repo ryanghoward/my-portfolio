5d9b3d24f85d3c503ded4e35bc8418e67bb86a73
Ryan Howard <ryanghoward@protonmail.com>
2022-04-25 21:41:57 -0700

    save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,16 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#001425]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <div>
+          <img
+            className='w-21 mx-auto rounded-full object-left'
+            src={Ryan}
+            alt='My face'
+          />
+        </div>
         <p className='text-[#96d8d8]'>Hello, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Ryan G. Howard
-          <img className='w-21 mx-auto rounded-2xl' src={Ryan} alt='My face' />
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Full Stack Developer.
