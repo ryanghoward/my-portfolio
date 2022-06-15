de5215423f18aaee2e0b4ec8ef3b138ac7b39f4c
Ryan Howard <ryanghoward@protonmail.com>
2022-06-15 11:00:55 -0700

    Added PostgreSQL to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,10 @@ import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
-import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
 import Redux from "../assets/redux.png";
+import PostgreSQL from "../assets/PostgreSQL.png";
+// import GitHub from "../assets/github.png";
 
 const Skills = () => {
   return (
@@ -48,8 +49,8 @@ const Skills = () => {
             <p className='my-4'>Tailwind</p>
           </div>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={GitHub} alt='HTML Icon' />
-            <p className='my-4'>GitHub</p>
+            <img className='w-20 mx-auto' src={PostgreSQL} alt='HTML Icon' />
+            <p className='my-4'>PostgreSQL</p>
           </div>
         </div>
       </div>
