4ac224a218d614d1c8c153358cf3c356ad41044c
Ryan Howard <ryanghoward@protonmail.com>
2022-06-15 09:49:26 -0700

    Added Water My Plants project to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-// import WorkImg from "../assets/workImg.jpeg";
-// import realEstate from "../assets/realestate.jpg";
-import construction from "../assets/construction.png";
+import WorkImg from "../assets/workImg.jpeg";
+import realEstate from "../assets/realestate.jpg";
+import WMP from "../assets/WMPhome.jpg";
+// import construction from "../assets/construction.png";
 
 const Work = () => {
   return (
@@ -11,26 +12,32 @@ const Work = () => {
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#96d8d8]'>
             Work
           </p>
-          {/* <p className='py-6'>Checkout some of my recent work</p> */}
-          <p className='py-6 text-center'>Coming Soon</p>
+          <p className='py-6'>Checkout some of my recent work!</p>
+          {/* <p className='py-6 text-center'>Coming Soon</p> */}
         </div>
-        <img className='w-50 h-100' src={construction} alt='construction' />
-        {/* <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        {/* <img className='w-50 h-100' src={construction} alt='construction' /> */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${WMP})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Water My Plants
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a
+                  href='https://water-my-plants-9-build-week.vercel.app'
+                  target='_blank'
+                >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a
+                  href='https://github.com/Build-Week-Water-My-Plants-8/front-end'
+                  target='_blank'
+                >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -148,7 +155,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
