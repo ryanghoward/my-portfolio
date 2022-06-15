f97d7f47ab692f60702ad5b46e0337b3368d6319
Ryan Howard <ryanghoward@protonmail.com>
2022-06-15 10:05:31 -0700

    Added Potluck Planner to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import WorkImg from "../assets/workImg.jpeg";
 import realEstate from "../assets/realestate.jpg";
 import WMP from "../assets/WMPhome.jpg";
+import PP from "../assets/PotluckPlanner.PNG";
 // import construction from "../assets/construction.png";
 
 const Work = () => {
@@ -46,20 +47,23 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${PP})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Potluck Planner
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a
+                  href='https://potluck1-front-end.vercel.app/'
+                  target='_blank'
+                >
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/Potluck-Planner-A' target='_blank'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
