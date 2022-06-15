b77897e55898e9cc5e2e490ba7f7eb9d175cbbd9
Ryan Howard <ryanghoward@protonmail.com>
2022-06-15 15:49:57 -0700

    Added email functionality to left side email tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#639FB6]'>
             <a
               className='flex justify-between items-center w-full text-gray-300 text-xl'
-              href='/'
+              href='mailto:ryghoward@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
