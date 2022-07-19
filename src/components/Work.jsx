d6da87be4e845a5879dd4cf7704b5334e9a00bb8
Ryan Howard <ryanghoward@protonmail.com>
2022-07-19 10:22:20 -0700

    Added VHCL to work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import WMP from "../assets/WaterMyPlants.PNG";
 import PP from "../assets/PotluckPlanner.PNG";
 import UDD from "../assets/UnderdogDevs.PNG";
+import VHCL from "../assets/VHCL.PNG";
 
 const Work = () => {
   return (
@@ -13,9 +14,33 @@ const Work = () => {
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#96d8d8]'>
             Work
           </p>
-          <p className='py-6'>Checkout some of my recent work!</p>
+          <p className='py-6'>
+            Checkout some of my recent work! More to come soon!
+          </p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div
+            style={{ backgroundImage: `url(${VHCL})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider text-center'>
+                VHCL (Turo Clone)
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://ryanghoward.github.io/VHCL/' target='_blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Site
+                  </button>
+                </a>
+                <a href='https://github.com/ryanghoward/VHCL' target='_blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${WMP})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -94,30 +119,29 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <p className='py-6'>More to come very soon!</p>
           {/* <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${VHCL})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
+          >
             <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+              <span className='text-2xl font-bold text-white tracking-wider text-center'>
+                VHCL (Turo Clone)
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://ryanghoward.github.io/VHCL/' target='_blank'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Site
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/ryanghoward/VHCL' target='_blank'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
               </div>
             </div>
-          </div>
-          <div
+          </div> */}
+          {/* <div
             style={{ backgroundImage: `url(${WorkImg})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
